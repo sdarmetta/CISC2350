72587010e4a5aa8d400714b0d6642c3730d75d5d
sdarmetta <31830985+sdarmetta@users.noreply.github.com>
2017-11-15 23:03:39 -0500

    Add files via upload

---
+++ HW12-1/scripts/script.js
@@ -0,0 +1,37 @@
+//testing console
+console.log("hello");
+
+//variables
+var atlas = 'Atlas Statue';
+var empireState = 'Empire State Building';
+var liberty = 'Statue of Liberty';
+
+//logging variables to console
+console.log(atlas);
+console.log(empireState);
+console.log(liberty);
+
+//w3resource.com
+//function to return random background color variable
+function randomBackGround(){
+	var x = Math.floor(Math.random() * 256);
+	var y = Math.floor(Math.random() * 256);
+	var z = Math.floor(Math.random() * 256);
+
+	var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+	console.log(bgColor);
+
+	return bgColor;
+	
+}
+
+//function to change background color continuously
+function changeBackGround(){
+	var newBackGround = document.getElementById("full");
+	var button = true;
+
+	console.log(newBackGround);
+
+	newBackGround.style.backgroundColor = randomBackGround();
+}
